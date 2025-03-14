a1b55b3fb2b32593296139fb8194171ae84605b2
Luigson <luigsongesse@gmail.com>
2025-03-14 14:56:52 -0300

    Adicionei as funções de salvar a senha em um arquivo password.txt e a função de listar as senhas geradas

--- Gerador.sh
+++ Gerador.sh
@@ -19,7 +19,22 @@ Opções:
 -h : Exibir esse menu
 
 O comportamento padrão do script é gerar uma senha de 8 caracteres minúsculos."
+
+elif [ "$1" = "-p" ]; then
+	echo "Senhas Geradas:"
+	cat password.txt
+
 fi
+
+#Script de Salvar a senha em um .txt
+if [ -e password.txt ]; then
+	echo "$senha" >> password.txt
+else
+	touch password.txt
+	echo "$senha" >> password.txt
+fi
+
+
 
 # Imprime a senha gerada
 # Adicione aqui também a implementação do código que salva a senha no passwords.txt e cria o arquivo caso ele não exista
