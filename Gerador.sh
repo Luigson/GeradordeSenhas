dd8ac98e890666066f7e9b809e0c356d6ddb95e8
Felipe Duarte <felipeaugustoduarte2@gmail.com>
2025-03-14 15:59:47 -0300

    Versão Final com criptografia de senha

--- Gerador.sh
+++ Gerador.sh
@@ -24,6 +24,16 @@ elif [ "$1" = "-c" ]; then
         # Essa linha limpa o arquivo password.txt
         > password.txt
 
+elif [ "$1" = "--crip" ]; then
+	senha=$(cat /dev/urandom | tr -dc "$CARACTERES" | fold -w 10 | head -n 1)
+	echo "$senha" >> senha.tmp
+	openssl enc -aes-256-cbc -salt -in senha.tmp -out password.enc
+	rm senha.tmp
+
+elif [ "$1" = "--decrip" ]; then
+	openssl enc -d -aes-256-cbc -salt -in password.enc -out password.dec
+	rm password.enc
+
 else
         # Essa linha gera uma senha quando o script é executado
        	senha=$(cat /dev/urandom | tr -dc "$CARACTERES" | fold -w 10 | head -n 1)
