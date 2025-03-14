cdb1bafab1ea07e58ae0f99e268ae73bd52a4226
Felipe Duarte <felipeaugustoduarte2@gmail.com>
2025-03-14 14:59:19 -0300

    Consertei problema na geração de senhas mesmo quando utilizado argumentos

--- Gerador.sh
+++ Gerador.sh
@@ -3,11 +3,7 @@
 # Definir conjuntos de caracteres
 CARACTERES="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?/~"
 
-# Essa linha gera uma senha quando o script é executado
-senha=$(cat /dev/urandom | tr -dc "$CARACTERES" | fold -w 10 | head -n 1)
-
 # Interpretar os argumentos:
-# (Complete o código com as outras funcionalidades usando o elif)
 if [ "$1" = "-h" ]; then
   echo -e "Bem vindo ao password-generator! Versão 1.0, (c) 2025, Fulano de Tal,DIMAp, UFRN
 Uso: ./password-generator.sh [OPÇÕES]:
@@ -21,11 +17,14 @@ Opções:
 O comportamento padrão do script é gerar uma senha de 10 caracteres."
 
 elif [ "$1" = "-c" ]; then
+	# Essa linha limpa o arquivo password.txt
 	> password.txt
+
+else
+	# Essa linha gera uma senha quando o script é executado
+	senha=$(cat /dev/urandom | tr -dc "$CARACTERES" | fold -w 10 | head -n 1)
+
 fi
 
-# Imprime a senha gerada
-# Adicione aqui também a implementação do código que salva a senha no passwords.txt e cria o arquivo caso ele não exista
-# Dica: lembre-se de utilizar o operador "-e" junto com a condicional "if"!
 echo "Senha gerada: $senha"
 
